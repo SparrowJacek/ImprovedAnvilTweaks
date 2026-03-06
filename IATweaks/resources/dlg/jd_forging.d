688a8d38172386df6e3a6a258e165845e3c2d144
SparrowJacek <jkozmicj@gmail.com>
2026-03-06 07:26:21 +0100

    Enable JD for all protagonists component

---
+++ IATweaks/resources/dlg/jd_forging.d
@@ -0,0 +1,41 @@
+
+// JD Sword without alignment/class restrictions
+ALTER_TRANS BOTSMITH
+  BEGIN 176 END
+  BEGIN 1 END
+BEGIN
+  "TRIGGER" ~Global("Iajdplot","GLOBAL",1)
+Dead("gorgit")
+Dead("S!dlord")
+PartyHasItem("S!misc07")
+PartyHasItem("S!misc17")
+PartyHasItem("S!misc06")
+PartyHasItem("S!misc08")
+PartyHasItem("S!misc02")
+PartyHasItem("sw1h60")
+PartyHasItem("scrl9x")
+PartyHasItem("scrl9d")
+PartyHasItem("scrlb4")
+Dead("fsdragon")
+PartyGoldGT(249999)~
+END
+
+ALTER_TRANS WSMITH01
+  BEGIN 73 END
+  BEGIN 1 END
+BEGIN
+  "TRIGGER" ~Global("Iajdplot","GLOBAL",1)
+Dead("gorgit")
+Dead("S!dlord")
+PartyHasItem("S!misc07")
+PartyHasItem("S!misc17")
+PartyHasItem("S!misc06")
+PartyHasItem("S!misc08")
+PartyHasItem("S!misc02")
+PartyHasItem("sw1h60")
+PartyHasItem("scrl9x")
+PartyHasItem("scrl9d")
+PartyHasItem("scrlb4")
+Dead("fsdragon")
+PartyGoldGT(249999)~
+END
